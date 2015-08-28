d107a40356f6b875b6172e1e789f16e368a939c3
Saskia Heckele <saskia.heckele@pentasys.de>
2015-08-28 17:21:48 +0200

    set and show marker on map

--- js/googleMaps.js
+++ js/googleMaps.js
@@ -1,4 +1,8 @@
 var map;
+var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var labelIndex = 0;
+
+
 function initAutocomplete() {
 	
 	// Initial Marker for the Map
@@ -61,6 +65,15 @@ function initAutocomplete() {
 		title: invernessTitle
 	});
 	
+	// add Marker to maps when Map is clicked
+	
+	// This event listener calls addMarker() when the map is clicked.
+	google.maps.event.addListener(map, 'click', function(event) {
+		addMarker(event.latLng, map);
+	});
+
+
+	
 	
 	/* ===========================================
 	 * ============ S E A R C H ==================
@@ -121,10 +134,19 @@ function initAutocomplete() {
 		
 		map.fitBounds(bounds);
 	});
-
-	
-	
-	
-
 }
+
+// Adds a marker to the map.
+function addMarker(location, map) {
+  // Add the marker at the clicked location, and add the next-available label
+  // from the array of alphabetical characters.
+  var marker = new google.maps.Marker({
+    position: location,
+    label: labels[labelIndex++ % labels.length],
+    map: map
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
 
